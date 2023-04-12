681c571d59ea7fa8f6e4d95647dc1a21fe2e5133
YberChambi <ichambi.pasantedcloud@datec.com.bo>
2023-04-12 10:27:26 -0400

    primer commit

---
+++ eje1/eje1.py
@@ -0,0 +1,10 @@
+n1 = input("¿Dime el primer número?")
+n1 = int(n1)
+n2 = input("¿Dime el segundo número?")
+n2 = int(n2)
+if n2>n1:
+    suma = n1 + n2
+    print ("la suma es")
+    print (suma)
+else:
+    print("el numero 2 no es mayor al numero1")
