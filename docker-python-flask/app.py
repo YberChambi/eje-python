1676705d3b87b5a4a24b69166bc9d04c37cb7ffb
YberChambi <ichambi.pasantedcloud@datec.com.bo>
2023-04-13 16:47:58 -0400

    ambiente ejercicios python

--- docker-python-flask/app.py
+++ docker-python-flask/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from FLASK and DOCKER</h2>'
+    return '<h1>Ejercicios python en FLASK WEB</h1>'
 
 
 if __name__ == "__main__":
