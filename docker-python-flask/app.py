58a5be1ba59b827fd3615ca9be2b8c541db5eef0
YberChambi <ichambi.pasantedcloud@datec.com.bo>
2023-04-13 17:05:55 -0400

    otro cambio ejercicios python

--- docker-python-flask/app.py
+++ docker-python-flask/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Ejercicios python en FLASK WEB</h1>'
+    return '<h1>Ejercicios python en FLASK WEB 2</h1>'
 
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return 'hola {0}!'.format(nombre)
 
 if __name__ == "__main__":
     app.run(debug=True)
